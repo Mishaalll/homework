2f203d224ddf64ae12eec9292bce649cbb582d08
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-09-26 22:47:50 +0300

    Create task12.c

---
+++ task12.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+    printf("Program: %s\n", argv[0]);
+        for (int i = 1; i < argc; i++) 
+        {
+            printf("Argument %d: %s\n", i, argv[i]);
+        }
+    
+    return 0;
+}
