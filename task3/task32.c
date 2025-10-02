6d76ad3b720d4cd85a1baad85f79b7c365f2d49b
Mishaalll <mikhail.ushakov.pp@gmail.com>
2025-10-02 19:48:56 +0300

    atoi

--- task3/task32.c
+++ task3/task32.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include "myatoi.h"
 
 int main(int argc, char *argv[]) 
 {
     int sum = 0;
     for (int i = 1; i < argc; ++i) 
     {
-        int val = atoi(argv[i]);
+        int val = myatoi(argv[i]);
         sum += val;
     }
     printf("Sum: %d\n", sum);
